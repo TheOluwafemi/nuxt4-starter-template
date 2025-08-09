b43e99901c0731f472096401c86cad8c064698f7
theoluwafemi <oluwafemidamilola21@gmail.com>
2025-08-09 23:34:10 +0100

    Initial commit - instal tailwind css support

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="text-white bg-blue-950 text-7xl">
+      Wwlcome to Nuxt 4 Starter Template
+    </div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
